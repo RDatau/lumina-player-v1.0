0b019ee4cf8ebffad3f3fe7c883b85ac84ce920d
RDatau <87753062+RDatau@users.noreply.github.com>
2026-02-12 22:35:20 +0800

    feat: Improve PWA offline and manifest assets
    Updates the service worker to correctly use `index.html` as the offline fallback page. Enhances the `manifest.json` with additional PNG icons, providing better display in various contexts and improving the PWA experience.

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,8 @@
 // This is the "Offline page" service worker
-
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 const CACHE = "pwabuilder-page";
-
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "ToDo-replace-this-name.html";
+const offlineFallbackPage = "index.html";
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
@@ -37,7 +34,6 @@ self.addEventListener('fetch', (event) => {
         const networkResp = await fetch(event.request);
         return networkResp;
       } catch (error) {
-
         const cache = await caches.open(CACHE);
         const cachedResp = await cache.match(offlineFallbackPage);
         return cachedResp;
